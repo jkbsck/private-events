ec9245d90224b260c54763d42168f4397151016e
jkbsck <sucik.jakub@gmail.com>
2020-09-13 17:13:12 +0200

    Fixed error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,16 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: 'You have signed up successfully.'
+    else
+      if @user.username == ''
+        redirect_to new_user_path, notice: "Username can not be empty!"
+      else
+        redirect_to new_user_path, notice: "User already exists!"
+      end
+    end
   end
 
   def show
