f72632e78b2a0ab0f219c02fc6c08869473a57f7
jkbsck <sucik.jakub@gmail.com>
2020-09-19 20:14:00 +0200

    Before rollback migration

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: :attended_event_id
-  has_many :attendees, through: :attendances
+  has_many :attendees, through: :attendances, foreign_key: :attendee_id #, source: :attendee
 end
