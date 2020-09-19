83c63608f7813a93f9222595e4b78c2d4a826e85
jkbsck <sucik.jakub@gmail.com>
2020-09-19 10:42:15 +0200

    before migration of through table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   has_many :events, foreign_key: :creator_id
-  has_many :attendances
-  has_many :attended_events, through: :attendances, source: :event
+  has_many :attendances, foreign_key: :attendee_id
+  has_many :attended_events, through: :attendances
 end
